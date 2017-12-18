5228d9223fc433438caefb9ee4066f77b94657c6
Richard Ho <richardpanda1993@gmail.com>
2017-12-17 21:35:53 -0800

    Add link to home in navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Toolbar, ToolbarRow, ToolbarTitle } from 'rmwc';
+
+import './style.css';
 
 class Navbar extends Component {
   render() {
     return (
       <Toolbar>
         <ToolbarRow>
-          <ToolbarTitle>Quick Poll</ToolbarTitle>
+          <ToolbarTitle><Link className="home-link" to="/">Quick Poll</Link></ToolbarTitle>
         </ToolbarRow>
       </Toolbar>
     );
