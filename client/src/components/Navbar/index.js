88ca17a043b0e02f1c7ab8c8164d6bc4d5ef8d5f
Richard Ho <richardpanda1993@gmail.com>
2017-12-21 10:19:24 -0800

    Remove unused imports

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import AppBar from 'react-toolbox/lib/app_bar/AppBar';
-import Button from 'react-toolbox/lib/button/Button';
-import Link from 'react-toolbox/lib/link/Link';
-import Navigation from 'react-toolbox/lib/navigation/Navigation';
 
 import './style.css';
 
